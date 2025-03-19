bc4c76f629b62cb782efc8ba98eead6461b2b7bd
Saraalkharawi <saraalkharawi@icloud.com>
2025-03-19 16:51:18 +0100

    fixed login

--- login.php
+++ login.php
@@ -7,12 +7,17 @@ $password = $_POST['password'];
 $sql = "SELECT * FROM User WHERE email='$email'";
 $result = $conn->query($sql);
 
+$user = $result->fetch_assoc();
+$hashedPassword = $user['Password'] ?? '';
+
 if ($result->num_rows > 0) {
     $user = $result->fetch_assoc();
-    if (password_verify($password, $user['password'])) {
+    if (password_verify($password, $hashedPassword)) {
         echo "Inloggning lyckades! <a href='studai.html'>Gå vidare</a>";
     } else {
-        echo "Fel lösenord!";
+        echo "Fel lösenord!<br>";
+        echo "Inmatat lösenord: " . htmlspecialchars($password) . "<br>";
+        echo "Hashat lösenord i databasen: " . $user . "<br>";    
     }
 } else {
     echo "Användaren finns inte!";
